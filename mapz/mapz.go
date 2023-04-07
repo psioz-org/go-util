a1ec2af7ba9143ddee7b704a53565278dd5e5350
Zev Zakaryan <nphprog@gmail.com>
2023-04-07 11:17:19 +0700

    feat: add mapz,slicez,stringz,zz

---
+++ mapz/mapz.go
@@ -0,0 +1,51 @@
+package mapz
+
+import (
+	"encoding/json"
+
+	"github.com/zev-zakaryan/go-util/structurez"
+)
+
+func Join(out map[string]string, excludeEmpty bool, in ...map[string]string) {
+	for _, in1 := range in {
+		for k, v := range in1 {
+			if _, ok := out[k]; !ok && (!excludeEmpty || v != "") {
+				out[k] = v
+			}
+		}
+	}
+}
+
+func ToMap(obj interface{}) map[string]interface{} {
+	var out map[string]interface{}
+	var objJ []byte
+	switch v := obj.(type) {
+	case string:
+		objJ = []byte(v)
+	case []byte:
+		objJ = v
+	default:
+		objJ, _ = json.Marshal(obj)
+	}
+	json.Unmarshal(objJ, &out)
+	return out
+}
+
+func ToStringMap(obj interface{}) map[string]string {
+	var outI map[string]interface{}
+	var objJ []byte
+	switch v := obj.(type) {
+	case string:
+		objJ = []byte(v)
+	case []byte:
+		objJ = v
+	default:
+		objJ, _ = json.Marshal(obj)
+	}
+	json.Unmarshal(objJ, &outI)
+	out := make(map[string]string)
+	for k, v := range outI {
+		out[k] = structurez.ToForce[string](v)
+	}
+	return out
+}
